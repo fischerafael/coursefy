9e1ed09df0c139f434d1877361bbc6f271d84590
Rafael Fischer <rafaelsanfischer@gmail.com>
2022-06-23 20:53:29 -0300

    feat: added header

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,14 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider>
+      <Head>
+        <title>CourseFy</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
       <Component {...pageProps} />
     </ChakraProvider>
   );
